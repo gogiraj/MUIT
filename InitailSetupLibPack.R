24a831225c4251f6f097befd1057dc905a1fe5f7
gogiraj <gogiraj@gmail.com>
2016-09-29 15:13:10 +0530

    This is for testing

---
+++ InitailSetupLibPack.R
@@ -0,0 +1,26 @@
+## install Packages 1) install 2)  library() 3. updateR()
+
+install.packages('installr')
+
+library('installr')
+
+## To update Package
+updateR()
+
+## install move update package
+
+if(!require(installr))
+{
+  install.packages('installr')
+  require(installr)
+}
+updateR(F,T,T,F,T,F,T)
+
+### to update
+
+update.packages(checkBuilt = TRUE,ask=FALSE)
+
+## Intsall Mutliple packages
+install.packages("caTools","devtools")
+
+# testing
